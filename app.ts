37c1c7c7e31f4da62b4fddb51f884a09e039c8ee
Ilan Frumer <ilanfrumer@gmail.com>
2022-05-31 16:13:56 +0300

    Deno Server example

---
+++ app.ts
@@ -0,0 +1,27 @@
+import { Application } from "oak";
+import { sleepRandomAmountOfSeconds } from "sleep";
+import { faker } from "faker";
+
+const app = new Application();
+const port = 8000;
+const hostname = "0.0.0.0";
+
+app.use(async (ctx) => {
+  await sleepRandomAmountOfSeconds(0, 1);
+  const users = Array(20)
+    .fill(null)
+    .map(() => ({
+      id: faker.random.uuid(),
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+    }));
+
+  ctx.response.body = users;
+});
+
+app.addEventListener("listen", () => {
+  console.log(`Listening on ${hostname}:${port}`);
+});
+
+await app.listen({ port, hostname });
